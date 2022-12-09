f3db30024fbf17e035d3a8b861feea7d2b8c4a5a
MRGrozan <108768290+MRGrozan@users.noreply.github.com>
2022-12-10 02:38:27 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let tg = window.Telegram.WebApp
+
+tg.expand();
+
+let item = "";
+
+let input1 = document.getElementsByName("fio")
+let input2 = document.getElementsByName("city")
+let input3 = document.getElementsByName("street")
+let input4 = document.getElementsByName("house")
+let input5 = document.getElementsByName("email")
+let input6 = document.getElementsByName("phone")
+let input7 = document.getElementsByName("info")
+
+Telegram.WebApp.onEvent("")
